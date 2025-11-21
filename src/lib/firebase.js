a558e0cd9f4ee59db11d9737901fda9e6b064ac4
Joseph Musenge <81922031+JosephMusenge@users.noreply.github.com>
2025-11-21 14:56:51 -0600

    updated the PostCSS configuration file and added Firebase initialization and code

---
+++ src/lib/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Firebase config from console
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCcXE_v3588zzO_gYFSZULJUHjiR1ob1Fo",
+    authDomain: "mind-bank-app.firebaseapp.com",
+    projectId: "mind-bank-app",
+    storageBucket: "mind-bank-app.firebasestorage.app",
+    messagingSenderId: "813262607244",
+    appId: "1:813262607244:web:e2507dbb067150a4613ca9",
+    measurementId: "G-0P2HCV7KYR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export the services so other files can use them
+export const auth = getAuth(app);
+export const db = getFirestore(app);
